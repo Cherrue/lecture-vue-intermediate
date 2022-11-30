d361cb141d7809a9f8d37d5f5a515633dc69045d
taehyung-lee <taehyung@nhn.com>
2022-11-30 17:03:42 +0900

    기본 컴포넌트 추가

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <!-- html -->
+  <div>
+    header
+  </div>
+</template>
+
+<script>
+export default {
+  // js
+  name: "TodoHeader"
+}
+</script>
+
+<style scoped>
+/* css */
+</style>
